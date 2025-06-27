f4699aed478418ad9d17278345e65b89d41af8db
LBC <541014133@qq.com>
2025-06-27 12:23:03 +0800

    refactor(knosphere-backend):模型配置绑定

--- src/main/java/com/shing/knospherebackend/config/EmbeddingModelConfig.java
+++ src/main/java/com/shing/knospherebackend/config/EmbeddingModelConfig.java
@@ -1,5 +1,6 @@
 package com.shing.knospherebackend.config;
 
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.context.annotation.Bean;
@@ -13,17 +14,17 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class EmbeddingModelConfig {
 
-    // 使用 DashScope 作为主用模型
-//    @Bean
-//    @Primary
-//    public EmbeddingModel primaryEmbeddingModel(DashscopeEmbeddingModel  dashscope) {
-//        return dashscope;
-//    }
+     // 使用 DashScope 作为主用模型
+    @Bean
+    @Primary
+    public EmbeddingModel primaryEmbeddingModel(DashScopeEmbeddingModel dashscope) {
+        return dashscope;
+    }
 
-     // 或者使用 Ollama 作为主用模型
-     @Bean
-     @Primary
-     public EmbeddingModel primaryEmbeddingModel(OllamaEmbeddingModel ollama) {
-        return ollama;
-     }
+//     // 或者使用 Ollama 作为主用模型
+//     @Bean
+//     @Primary
+//     public EmbeddingModel primaryEmbeddingModel(OllamaEmbeddingModel ollama) {
+//        return ollama;
+//     }
 }
